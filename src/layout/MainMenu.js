c0fba308ba5e71f844fbaed59e44de8d92e08fd2
SoftwareTalent <mobile_top@hotmail.com>
2022-06-15 11:15:10 -0500

    header edit

--- src/layout/MainMenu.js
+++ src/layout/MainMenu.js
@@ -1,19 +1,29 @@
-import {Link, useLocation} from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const ROUTES = [
-    {title:"Overview", path:"/overview"},
-    {title:"Faq",path:'/fag'},
-    {title:"Road Map",path:'/roadmap'},
-    {title:"Polium Pass",path:'/polium-pass'}
-]
+  //   { title: 'Overview', path: '/overview' },
+  //   { title: 'Faq', path: '/fag' },
+  //   { title: 'Road Map', path: '/roadmap' },
+  //   { title: 'Polium Pass', path: '/polium-pass' },
+  { title: 'Overview', path: '/' },
+  { title: 'Faq', path: '/' },
+  { title: 'Road Map', path: '/' },
+  { title: 'Polium Pass', path: '/' },
+];
 
-export default function MainMenu(){
-    const {pathname} = useLocation();
-    return(
-        <div className="hidden sm:flex gap-4">
-            {ROUTES.map((router,index)=>(
-                <Link className={`${pathname === router.path ?'text-accent':''}`} to = {router.path} key={index}>{router.title}</Link>
-            ))}
-        </div>
-    )
+export default function MainMenu() {
+  const { pathname } = useLocation();
+  return (
+    <div className='hidden sm:flex gap-4'>
+      {ROUTES.map((router, index) => (
+        <Link
+          className={`${pathname === router.path ? 'text-accent' : ''}`}
+          to={router.path}
+          key={index}
+        >
+          {router.title}
+        </Link>
+      ))}
+    </div>
+  );
 }
